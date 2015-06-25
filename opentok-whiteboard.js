dc50fda4c17332963df84447ea600de028fa5642
Suman Saurabh <ss.sumansaurabh92@gmail.com>
2015-06-25 18:02:09 +0530

    Removed redo

--- opentok-whiteboard.js
+++ opentok-whiteboard.js
@@ -23,20 +23,20 @@ var OpenTokWhiteboard = angular.module('opentok-whiteboard', ['opentok'])
                 '<input type="button" ng-click="erase()" ng-class="{OT_erase: true, OT_selected: erasing}"' +
                 ' value="Eraser"></input>' +
 
-                '<input type="button" ng-click="capture()" class="OT_capture" value="{{captureText}}"><a download=\'HomeLane.png\' href=\'{{imageName}}\'></a></input>' +
+                '<a style="text-decoration: none !important;" id="download"><input type="button" ng-click="capture()" class="OT_capture" value="{{captureText}}"></input></a>'+
 
                 '<input type="button" ng-click="clear()" class="OT_clear" value="Clear"></input>' +
 
-                '<input type="button" ng-click="undo()" class="OT_undo" value="Undo"></input>' +
+                '<input type="button" ng-click="undo()" class="OT_undo" value="Undo"></input>' /*+
 
                 '<input type="button" ng-click="redo()" class="OT_redo" value="Redo"></input>' +
-                '<input type="file" class="OT_loadImage" id="OT_loadImage" value="Load\nImage"></input>',
+                '<input type="file" class="OT_loadImage" id="OT_loadImage" value="Load\nImage"></input>'*/,
 
             link: function (scope, element, attrs) {
                 var canvas = element.context.querySelector("canvas"),
                     select = element.context.querySelector("select"),
                     input = element.context.querySelector("input"),
-                    inputImage = element.context.querySelector("a"),
+                    inputImage = element.context.querySelector("#download"),
                     inputFile = element.context.querySelector("input.OT_loadImage"),
                     client = {
                         dragging: false
@@ -52,7 +52,8 @@ var OpenTokWhiteboard = angular.module('opentok-whiteboard', ['opentok'])
                     drawHistoryReceived,
                     batchUpdates = [],
                     iOS = /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
-
+				var mainuser=1;
+				var imgobj;
                 ctx.lineCap = "round";
                 ctx.fillStyle = "solid";
 
@@ -68,31 +69,13 @@ var OpenTokWhiteboard = angular.module('opentok-whiteboard', ['opentok'])
                     },
                     {
                         'background-color': 'green'
-                    },
-                    {
-                        'background-color': 'orange'
-                    },
-                    {
-                        'background-color': 'purple'
-                    },
-                    {
-                        'background-color': 'brown'
                     }];
                 scope.captureText = iOS ? 'Email' : 'Capture';
 
                 canvas.width = attrs.width || element.width();
                 canvas.height = attrs.height || element.height();
 
-                var clearCanvas = function () {
-                    ctx.save();
-
-                    // Use the identity matrix while clearing the canvas
-                    ctx.setTransform(1, 0, 0, 1, 0, 0);
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-                    // Restore the transform
-                    ctx.restore();
-                };
+                
 
                 scope.changeColor = function (color) {
                     scope.color = color['background-color'];
@@ -105,7 +88,6 @@ var OpenTokWhiteboard = angular.module('opentok-whiteboard', ['opentok'])
                 scope.clear = function () {
                     clearCanvas();
                     clearStack();
-                    clear
                     if (OTSession.session) {
                         OTSession.session.signal({
                             type: 'otWhiteboard_clear'
@@ -124,12 +106,8 @@ var OpenTokWhiteboard = angular.module('opentok-whiteboard', ['opentok'])
                         // On iOS you can put HTML in a mailto: link
                         window.location.href = "mailto:?subject=Whiteboard&Body=<img src='" + canvas.toDataURL('image/png') + "'>";
                     } else {
-                        // We just open the image in a new window
-                        // here is the most important part because if you dont replace you will get a DOM 18 exception.
-                        //var imghref=canvas.toDataURL("image/jpeg").replace("image/png", "image/octet-stream");
-
-                        //scope.imageName=imghref;
-                        window.open(canvas.toDataURL('image/png'));
+						inputImage.href=canvas.toDataURL('image/png');
+						inputImage.download="homelane.png";
                     }
                 };
 
@@ -154,89 +132,78 @@ var OpenTokWhiteboard = angular.module('opentok-whiteboard', ['opentok'])
                         drawHistory.push(update);
                     });
                 };
+				
+				var clearCanvas = function () {
+                    ctx.save();
 
+                    // Use the identity matrix while clearing the canvas
+                    ctx.setTransform(1, 0, 0, 1, 0, 0);
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                    // Restore the transform
+                    ctx.restore();
+                };
 
                 /*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
                 scope.undo = function () {
                     if (!undoStack.length)
                         return;
                     undodata = undoStack.pop();
-
                     undoWhiteBoard(undodata, 1);
-
-                    //for(i=0;i<undoStack[undoStack.length-1];i++)
-                    //redoStackData.push(drawHistory.pop());
                     redoStack.push(undodata);
                     sendUpdate('otWhiteboard_undo', undodata);
-                    /*
-				if (OTSession.session) {
-                    batchUpdates.push(undodata);
-                    if (!updateTimeout) {
-                        updateTimeout = setTimeout(function () {
-                            batchSignal('otWhiteboard_undo', batchUpdates);
-                            batchUpdates = [];
-                            updateTimeout = null;
-                        }, 100);
-                    }
-                }*/
-                    //batchSignal('otWhiteboard_undo', redoStack[redoStack.length-1]);
                 };
                 var undoWhiteBoard = function (data, flag) {
-                        if (flag) {
-                            drawHistory.splice(data.start - data.count, data.count).forEach(function (update) {
-                                redoStackData.push(update);
-                            });
-                        } else
-                            drawHistory.splice(data.start - data.count, data.count);
-                        //console.log(redoStackData);
-                        /*
-				while(data--)
-					flag?redoStackData.push(drawHistory.pop()):drawHistory.pop();*/
-                        clearCanvas();
-                        drawHistory.forEach(function (update) {
-                            draw(update);
-                        });
-                    }
+					if (flag) {
+						drawHistory.splice(data.start - data.count, data.count).forEach(function (update) {
+							redoStackData.push(update);
+						});
+					} else{
+						data.forEach(function (update) {
+							drawHistory.splice(update.start - update.count, update.count);
+						});
+					}
+					clearCanvas();
+					drawHistory.forEach(function (update) {
+						draw(update);
+					});
+				}
                     /*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
-
+				/*
                 scope.redo = function () {
                     if (!redoStack.length)
                         return;
-
+					var position= redoStack[redoStack.length - 1].start-redoStack[redoStack.length - 1].count;
+					var data=redoStackData.splice(redoStackData.length-redoStack[redoStack.length - 1].count,redoStack[redoStack.length - 1].count);
+					//redoWhiteBoard(data,position);
+					
+					data.forEach(function (datapoints)){
+						redodata={pos: position++, update: datapoints};
+						redoWhiteBoard(redodata);
+						sendUpdate('otWhiteboard_redo', redodata);
+					}
+					/*
                     for (i = 0; i < redoStack[redoStack.length - 1].count; i++) {
                         update = redoStackData.pop();
                         draw(update);
                         drawHistory.push(update);
                         sendUpdate('otWhiteboard_update', update);
-                    }
+                    //}
                     undoStack.push(redoStack.pop());
 
                 };
-
-
-
-                var batchSignal = function (type, data, toConnection) {
-                    // We send data in small chunks so that they fit in a signal
-                    // Each packet is maximum ~250 chars, we can fit 8192/250 ~= 32 updates per signal
-                    var dataCopy = data.slice();
-                    var signalError = function (err) {
-                        if (err) {
-                            TB.error(err);
-                        }
-                    };
-                    while (dataCopy.length) {
-                        var dataChunk = dataCopy.splice(0, Math.min(dataCopy.length, 32));
-                        var signal = {
-                            type: type,
-                            data: JSON.stringify(dataChunk)
-                        };
-                        if (toConnection) signal.to = toConnection;
-                        OTSession.session.signal(signal, signalError);
-                    }
-                };
-
-                var updateTimeout;
-                var sendUpdate = function (type, update) {
+				var redoWhiteBoard = function(redodata){
+					drawHistory.splice(redodata.pos,0,redodata.update);
+					//drawHistory.splice.apply(drawHistory,[position, 0].concat(updates));
+					clearCanvas();
+					drawHistory.forEach(function (update) {
+                        draw(update);
+                    });
+					
+					
+				}
+				*/
+				var sendUpdate = function (type, update) {
                     if (OTSession.session) {
                         batchUpdates.push(update);
                         if (!updateTimeout) {
@@ -248,13 +215,49 @@ var OpenTokWhiteboard = angular.module('opentok-whiteboard', ['opentok'])
                         }
                     }
                 };
+                var batchSignal = function (type, data, toConnection) {
+                    // We send data in small chunks so that they fit in a signal
+                    // Each packet is maximum ~250 chars, we can fit 8192/250 ~= 32 updates per signal
+					if( mainuser) {
+						localStorage.setItem('wb-drawHistory', JSON.stringify(drawHistory));
+						localStorage.setItem('wb-undoStack', JSON.stringify(undoStack));
+						localStorage.setItem('wb-redoStack', JSON.stringify(redoStack));
+						localStorage.setItem('wb-redoStackData', JSON.stringify(redoStackData));
+					}
+					
+                    var dataCopy = data.slice();
+					
+                    var signalError = function (err) {
+                        if (err) {
+                            TB.error(err);
+                        }
+                    };
+                    while (dataCopy.length) {
+						
+                        var dataChunk = dataCopy.splice(0, Math.min(dataCopy.length, 32));
+                        var signal = {
+                            type: type,
+                            data: JSON.stringify(dataChunk)
+                        };
+
+                        if (toConnection) signal.to = toConnection;
+						console.log("yaha be gadbad3");
+                        OTSession.session.signal(signal, signalError);
+																		
+
+                    }
+                };
+
+                var updateTimeout;
+                
 
                 var clearStack = function () {
                     drawHistory = [];
                     undoStack = [];
                     redoStack = [];
                     redoStackData = [];
-                    checkpoints = 0;
+					start=0;
+					count=0;
                 }
 
 
@@ -262,15 +265,27 @@ var OpenTokWhiteboard = angular.module('opentok-whiteboard', ['opentok'])
                 angular.element(inputFile).on('change', function (event) {
                     var reader = new FileReader();
                     reader.onload = function (e) {
-                        var img = new Image();
+                        img = new Image();
                         img.onload = function () {
 
                             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-                            //var imgobj=canvas.toDataURL("image/png").replace(/^data:image\/(png|jpg);base64,/, "");
-                            /*
+							
+                            var imgobj=canvas.toDataURL("image/png").splice();
+							console.log(imgobj.length);
+							//while(imgobj.length){
+								
+							//	var datachunk
+							//}
+							
+							//console.log(imgobj);
+							
+							
+                            
 						if (OTSession.session) {
-							batchUpdates.push();
+							console.log("yaha be gadbad1");
+							batchUpdates.push(imgobj);
+							console.log("yaha be gadbad2");
 							
 							if (!updateTimeout) {
 								updateTimeout = setTimeout(function () {
@@ -279,7 +294,7 @@ var OpenTokWhiteboard = angular.module('opentok-whiteboard', ['opentok'])
 									updateTimeout = null;
 								}, 100);
 							}
-						}*/
+						}
                         };
                         img.src = e.target.result;
                     }
@@ -343,7 +358,6 @@ var OpenTokWhiteboard = angular.module('opentok-whiteboard', ['opentok'])
                                 };
 
                                 count++;
-
                                 draw(update);
                                 drawHistory.push(update);
                                 client.lastX = x;
@@ -380,6 +394,8 @@ var OpenTokWhiteboard = angular.module('opentok-whiteboard', ['opentok'])
                         },
                         'signal:otWhiteboard_undo': function (event) {
                             if (event.from.connectionId !== OTSession.session.connection.connectionId) {
+								//console.log("suman");
+								//console.log(JSON.parse(event.data.start));
                                 undoWhiteBoard(JSON.parse(event.data), 0);
                                 scope.$emit('otWhiteboardUpdate');
                             }
@@ -412,10 +428,15 @@ var OpenTokWhiteboard = angular.module('opentok-whiteboard', ['opentok'])
                         },
                         /*****************************************************/
                         connectionCreated: function (event) {
+							
                             if (drawHistory.length > 0 && event.connection.connectionId !==
                                 OTSession.session.connection.connectionId) {
+								console.log(OTSession.session.connection.connectionId);
+								mainuser=0;
                                 batchSignal('otWhiteboard_history', drawHistory, event.connection);
                             }
+							
+								
                         }
                     });
                 }
